d7c06a92380326021b12393a872356491b1a313c
sujan765 <B_K-s1@ulster.ac.uk>
2026-08-06 02:25:05 +0100

    adding  click interaction to service cards

---
+++ js/services.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".service-card").click(function() {
+        var name = $(this).find("h3").text();
+        var price = $(this).find("span").text();
+        alert("You picked " + name + " - " + price);
+    });
+});
